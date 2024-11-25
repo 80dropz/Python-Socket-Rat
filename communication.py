79c76b133e25f59b75bd4ccf5c6bf79f8c7ee3db
80dropz <153262187+80dropz@users.noreply.github.com>
2024-11-25 14:06:25 -0500

    Update communication.py

--- communication.py
+++ communication.py
@@ -2,6 +2,17 @@ import requests
 import socket
 import threading
 
+user_connections = []
+
+
+
+def mainmenu():
+    print(f"{user_connections[0]}: 1")
+    connectionbranch = input("enter the connection you want")
+    if connectionbranch == 1 or "1":
+        print(f"trying to connect to {user_connections[0]}")
+        communication(user_connections[0])
+
 def handle_client(client_socket):
     while True:
 
@@ -12,12 +23,11 @@ def handle_client(client_socket):
         print(f"Received from client: {data.decode()}") 
         client_socket.sendall(data) 
 
-
-
-
     client_socket.close()
 
 def server():
+    global s
+    global client_socket
     link = "https://raw.githubusercontent.com/80dropz/communication/refs/heads/main/ipv4.txt"
     data = requests.get(link)
     hostip = data.text.strip() 
@@ -32,16 +42,20 @@ def server():
 
     client_socket, addr = s.accept()
     print(f"Connection received from {addr}")
+    userlist = client_socket.recv(1024).decode()
+    user = userlist, client_socket
+    user_connections.append((user))
+    print(user_connections)
+    mainmenu()
 
-    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-    client_thread.start()
 
+
+def communication(socket):
     while True:
         command = input("Enter a command to send to the client (or type 'exit' to close): ")
         if command.lower() == 'exit':
             print("Closing connection.")
-            client_socket.close()
-            break
+            mainmenu()
         client_socket.sendall(command.encode())
 
         response = client_socket.recv(1024)
